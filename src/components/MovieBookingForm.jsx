fd58aabed8321b9a5b7744e6be4e0e4f063c9f32
amitmaurya <amitmaurya@sorigin.com>
2023-06-09 11:39:02 +0530

    Some Change on form

--- src/components/MovieBookingForm.jsx
+++ src/components/MovieBookingForm.jsx
@@ -6,58 +6,58 @@ function MovieBookingForm() {
  
   return (
     <>
-    <div id="booking" class="section">
-		<div class="section-center">
-			<div class="container">
-				<div class="row">
-					<div class="booking-form">
-						<div class="booking-bg"></div>
+    <div id="booking" classname="section">
+		<div classname="section-center">
+			<div classname="container">
+				<div classname="row">
+					<div classname="booking-form">
+						<div classname="booking-bg"></div>
 						<form>
-							<div class="form-header">
+							<div classname="form-header">
 								<h2>Book Your Seat</h2>
 							</div>
-							<div class="form-group">
-								<input class="form-control" type="text" placeholder="Enter your Name"/>
-								<span class="form-label">Name</span>
+							<div classname="form-group">
+								<input classname="form-control" type="text" placeholder="Enter your Name"/>
+								<span classname="form-label">Name</span>
 							</div>
-							<div class="form-group">
-								<input class="form-control" type="tel" placeholder="Enter your Phone number"/>
-								<span class="form-label">Phone</span>
+							<div classname="form-group">
+								<input classname="form-control" type="tel" placeholder="Enter your Phone number"/>
+								<span classname="form-label">Phone</span>
 							</div>
-							<div class="form-group">
-								<input class="form-control" type="tel" placeholder="Enter an origin location"/>
-								<span class="form-label">Pickup Location</span>
+							<div classname="form-group">
+								<input classname="form-control" type="tel" placeholder="Enter an origin location"/>
+								<span classname="form-label">Pickup Location</span>
 							</div>
-							<div class="form-group">
-								<input class="form-control" type="tel" placeholder="Enter a destination location"/>
-								<span class="form-label">Destination Location</span>
+							<div classname="form-group">
+								<input classname="form-control" type="tel" placeholder="Enter a destination location"/>
+								<span classname="form-label">Destination Location</span>
 							</div>
-							<div class="form-group">
-								<select class="form-control" required>
+							<div classname="form-group">
+								<select classname="form-control" required>
 									<option value="" label="&nbsp;" selected hidden></option>
 									<option>1</option>
 									<option>2</option>
 									<option>3</option>
 								</select>
-								<span class="select-arrow"></span>
-								<span class="form-label">Passenger Numbers</span>
+								<span classname="select-arrow"></span>
+								<span classname="form-label">Passenger Numbers</span>
 							</div>
-							<div class="row">
-								<div class="col-md-6">
-									<div class="form-group">
-										<input class="form-control" type="date" required/>
-										<span class="form-label">Pickup Date</span>
+							<div classname="row">
+								<div classname="col-md-6">
+									<div classname="form-group">
+										<input classname="form-control" type="date" required/>
+										<span classname="form-label">Pickup Date</span>
 									</div>
 								</div>
-								<div class="col-md-6">
-									<div class="form-group">
-										<input class="form-control" type="time" required/>
-										<span class="form-label">Pickup Time</span>
+								<div classname="col-md-6">
+									<div classname="form-group">
+										<input classname="form-control" type="time" required/>
+										<span classname="form-label">Pickup Time</span>
 									</div>
 								</div>
 							</div>
-							<div class="form-btn">
-								<button class="submit-btn">Book Now</button>
+							<div classname="form-btn">
+								<button classname="submit-btn">Book Now</button>
 							</div>
 						</form>
 					</div>
